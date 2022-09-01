527cf14fe4a6cfaaf433f98a27f53a84092c621d
snehamaddali <snehamaddali.1607@gmail.com>
2022-09-01 17:12:24 +0530

    This is part of testing perl script

--- Demo.py
+++ Demo.py
@@ -1,5 +1,1 @@
 print("Hello, world!")
-
-
-#this is a comment
-#trying to create this file in feature branch and merge it to the master 
--- print_hello.py
+++ print_hello.py
@@ -1,2 +1,4 @@
 print("hello World!")
 #This is present in the new branch
+#This is comment1 
+#This is comment 2 
